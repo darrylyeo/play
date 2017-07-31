b894048ecd9972387a0fc3588bbf1553b86492b2
Darryl Yeo <darryl@darryl-yeo.com>
2017-07-31 08:24:33 -0700

    Add Processing.js direction constants, font function

--- p5/libraries/processing-port.js
+++ p5/libraries/processing-port.js
@@ -4,3 +4,10 @@ p5.prototype.pushMatrix = p5.prototype.pushStyle = p5.prototype.push
 p5.prototype.popMatrix = p5.prototype.popStyle = p5.prototype.pop
 
 var PVector = p5.Vector
+
+var LEFT = 'left'
+var RIGHT = 'right'
+var UP = 'up'
+var DOWN = 'down'
+
+var createFont = name => new p5.Font(name)
